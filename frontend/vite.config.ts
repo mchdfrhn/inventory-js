3c10b951def8033277dbcd335bc4767dd86e06ff
Mochamad Farhan Ali <77632041+mchdfrhn@users.noreply.github.com>
2025-07-24 13:39:04 +0700

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'inventory-js-production.up.railway.app',
         changeOrigin: true,
       }
     }
